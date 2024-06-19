655541c825eb61bcdfe6d7e4c2b49406c76b9619
Martin A <martin.apalkov@wisercat.eu>
2024-06-19 14:57:29 +0300

    initial commit

---
+++ src/test/java/weather/weatherbackend/WeatherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package weather.weatherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
