6766097936e3a826986237f532be01b6f02b70cb
Martin A <martin.apalkov@gmail.com>
2024-06-27 11:11:35 +0300

    cors, getter setter

--- src/main/java/weather/weatherbackend/entity/Place.java
+++ src/main/java/weather/weatherbackend/entity/Place.java
@@ -23,4 +23,59 @@ public class Place {
 
     private LocalDate date;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhenomenonNight() {
+        return phenomenonNight;
+    }
+
+    public void setPhenomenonNight(String phenomenonNight) {
+        this.phenomenonNight = phenomenonNight;
+    }
+
+    public String getPhenomenonDay() {
+        return phenomenonDay;
+    }
+
+    public void setPhenomenonDay(String phenomenonDay) {
+        this.phenomenonDay = phenomenonDay;
+    }
+
+    public Integer getTempmin() {
+        return tempmin;
+    }
+
+    public void setTempmin(Integer tempmin) {
+        this.tempmin = tempmin;
+    }
+
+    public Integer getTempmax() {
+        return tempmax;
+    }
+
+    public void setTempmax(Integer tempmax) {
+        this.tempmax = tempmax;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
